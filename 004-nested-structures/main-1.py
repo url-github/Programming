8d857fb8a2b6b64c7b33eafd7f3b588d4c538104
Piotr <pmackowka@gmail.com>
2024-11-02 18:29:33 +0100

    004-nested-structures

---
+++ 004-nested-structures/main-1.py
@@ -0,0 +1,16 @@
+L = [[34, False], [0], [('abc', 123), {'a': 1, 'x': (True, 'schowany', 5)}]]
+
+print(L)
+
+print("###")
+
+L[2]
+print((L[2][1]['x'][1]))
+
+print("###")
+
+# Z poniższej tupli wypisz słowo: 'wróbel'.
+
+K = (('król', {2:'królewna', 1: ['córka', 'wróbel']},'5'),('żółw', 'wiewiórka'))
+
+print(K[0][1][1][1])
