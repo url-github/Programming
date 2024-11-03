b8cc083f8e858159aeead817b26e2969e7ef3ff4
Piotr <pmackowka@gmail.com>
2024-11-03 13:02:21 +0100

    013-slice

---
+++ 012-range/main-2.py
@@ -0,0 +1,5 @@
+# Stwórz listę R zawierającą co drugą liczbę z zakresu od -5 do 5 (włącznie).
+
+R = list(range(-5, 6, 2))
+
+print(R)
---
+++ 013-slice/main-2.py
@@ -0,0 +1,5 @@
+# Wypisz co trzecią literę stringa a, zaczynając od przedostatniej i cofając się do poczatku.
+
+a = '!ooe&sj7?czaa()lmxuo,t2fa^4rtngk'
+
+print(a[-2::-3])
