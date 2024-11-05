9b17dc4fce983745b1b23d276d302b40c501a6c5
Piotr <pmackowka@gmail.com>
2024-11-05 19:44:59 +0100

    023-copy-lists

---
+++ 023-copy-lists/main-2.py
@@ -0,0 +1,15 @@
+# Skopiuj zawartość listy L do zmiennej K
+# W liście L pod indeksem 1 umieść stringa 'jeden'
+# W liście K pod indeksem 1 umieść stringa 'JEDEN'
+# Wydrukuj obie listy w osobnych linijkach
+
+L = [0, 1, 2, 3]
+
+K = L[:]
+
+L[1] = 'jeden'
+
+K[1] = 'JEDEN'
+
+print(L)
+print(K)
