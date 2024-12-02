5c6bdfbd4365160ecf15d10d20f17c96465d1b6c
Piotr <pmackowka@gmail.com>
2024-12-02 08:45:21 +0100

    046-dir-help

--- 046-dir-help/main-1.py
+++ 046-dir-help/main-1.py
@@ -1,12 +1,15 @@
 # Pytanie 47 - do czego w Pythonie służą funkcje dir() i help().
 
 print(dir())
+# ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
+print("-"*30)
 
 if __name__ == '__main__':  # blok sprawdzający czy uruchamiamy dany plik bezpośrednio
     pass                    # plik uruchomiony bezpośrednio w atrybucie __name__ ma stringa '__main__'
                             # plik uruchomiony po zaimportowaniu do innego pliku w atrybucie __name__ ma swoją własną nazwę
 
 print(__file__)
+# /Users/p/Documents/Scripts/Programming/046-dir-help/main-1.py
 
 # dir(obiekt) - wyświetla listę atrybutów danego obiektu
 # help(obiekt.metoda) - wyświetla opis danego atrybutu obiektu
