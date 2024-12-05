a55016b6ff96f44bdd2cba1fb55246523310c28b
Piotr <pmackowka@gmail.com>
2024-12-05 11:20:58 +0100

    049-for-loop

--- 049-for-loop/main-3.py
+++ 049-for-loop/main-3.py
@@ -5,11 +5,13 @@ for j in range(1,i+1):
     result *= j
 
 print(i, result)
+# 10 3628800
 
 print('------------')
 
 import math
 print(10, math.factorial(10))
+# 10 3628800
 
 print('------------')
 
@@ -21,6 +23,16 @@ for i in range(1, x+1):
         result *= j
 
     print(i, result)
+# 1 1
+# 2 2
+# 3 6
+# 4 24
+# 5 120
+# 6 720
+# 7 5040
+# 8 40320
+# 9 362880
+# 10 3628800
 
 print('------------')
 
