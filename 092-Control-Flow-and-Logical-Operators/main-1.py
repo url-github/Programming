f9c8ea2edeb985d74d6ee67679ada23828a27f42
Piotr <pmackowka@gmail.com>
2024-12-30 12:06:32 +0100

    092-Control-Flow-and-Logical-Operators

---
+++ 092-Control-Flow-and-Logical-Operators/main-1.py
@@ -0,0 +1,11 @@
+# print(10 / 2)
+# print(10 // 2)
+# print(10 % 1)
+print(10 % 3)
+print(10 / 3)
+
+digits = input("Enter a number: ")
+if int(digits) % 2 == 0:
+	print("The number is even.")
+else:
+	print("The number is odd.")
---
+++ 092-Control-Flow-and-Logical-Operators/main-2.py
@@ -0,0 +1,18 @@
+wzrost = 130
+lat = 16
+
+
+if wzrost > 120:
+	print("Jedziesz na rollercoasterze!")
+	if lat <= 12:
+		print("Płacisz 5 zł.")
+	elif lat <= 18:
+		print("Płacisz 7 zł.")
+	else:
+		print("Płacisz 10")
+else:
+	print("Za niski na rollercoaster.")
+
+# The output will be:
+# Jedziesz na rollercoasterze!
+
